99e0e3cdc1caa2442d337c010a8527ca295e9099
Reut Ksenia <rieut.ksenia@mail.ru>
2018-09-09 10:40:50 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Я хочу изучать C#.");
+            Console.WriteLine("Я хочу изучать C#!!!");
 
             Console.ReadLine();
         }
